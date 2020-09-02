180be7f012ca1a20b4ee7a8b1de4c6a5a2e781a8
EphafroditeMin <vhf201@naver.com>
2020-09-02 22:19:46 +0900

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,110 +1,111 @@
- 
-  
- 
 import os
 import csv
 import requests
 from bs4 import BeautifulSoup
 
-os.system("clear")
-alba_url = "http://www.alba.co.kr"
+hacker_url = "https://news.ycombinator.com"
 
 
-
-result = requests.get(alba_url)
-soup = BeautifulSoup(result.text, "html.parser")
-super_brand = soup.find("div", {"id":
-"MainSuperBrand"}).find_all("span", {"class":"company"})
-jobs = []
-for i in super_brand:
-  i = i.string
-  try:
-    if i != None:
-      jobs.append(i)
-  except:
-    TypeError
-
-
-brand = soup.find("div", {"id":"MainSuperBrand"}).find_all("a",{"class":"goodsBox-info","href":True})
-site_urls = []
-for i in brand:
-  key = i['href']
-  site_urls.append(key)
-
-for w,y in zip(jobs,site_urls):
-  new_result = requests.get(y)
-  new_soup = BeautifulSoup(new_result.text, "html.parser")
-  albas = new_soup.find("tbody")
-  location = albas.find_all("td", {"class":"local first"})
-  LOCATION = []
-  for i in location:
-    if i != None:
-      i = i.text
-      LOCATION.append(i)
-    else:
-      LOCATION.append(None)
-
-  COMPANY = []
-  company = albas.find_all("span",{"class":"company"})
-  for i in company:
-    if i != None:
-      i = i.text
-      COMPANY.append(i)
-    else:
-      COMPANY.append(None)
-
-  HOUR = []
-  hour = albas.find_all("span",{"class":"time"})
-  for i in hour:
-    if i != None:
-      i = i.text
-      HOUR.append(i)
-    else:
-      HOUR.append(None)
-
-  PAY = []
-  pay = albas.find_all("td",{"class":"pay"})
-  for i in pay:
-    if i != None:
-      i = i.text
-      PAY.append(i)
-    else:
-      PAY.append(None)
-
-
-  UPLOADED = []
-  uploaded = albas.find_all("td", {"class":"regDate last"})
-  for i in uploaded:
-    if i != None:
-      i = i.text
-      UPLOADED.append(i)
-    else:
-      UPLOADED.append(None)
-
-
+def extract_infos():
+  result = requests.get(hacker_url)
+  soup = BeautifulSoup(result.text, "html.parser")
+  all_news = soup.find_all("tr", {"class":"athing"})
+  titles = []
+  values = []
+  for i in all_news:
+    news = i.find_all("td", {"class":"title"})
+    for x in news:
+      title_link = x.find("a")
+      try:
+        values.append(title_link)
+        each_title = title_link.string
+        titles.append(each_title)
+      except:
+        ValueError
+  values = values[1::2]
+  links = []
+  for q in values:
+    a = q.get("href")
+    links.append(a)
+  title_and_link =[]
+  for w,z in zip(titles, links):
+    my_dict = {"Title":w, "Links":z}
+    title_and_link.append(my_dict)
+  
+  subtext = soup.find_all("td", {"class":"subtext"})
+  POINT = []
+  USER_NAME = []
+  COMMENTS_NUM = []
+  COMMENTS_LINK = []
+  SHORT_LINK = []
+  for k in subtext:
+#    try:
+#      if sub_info != None:   
+    point = k.find("span", {"class":"score"})
+    try:
+      if point != None:
+        point = point.string
+        POINT.append(point)
+      else:
+        point = None
+        POINT.append(point)
+    except:
+      TypeError
+    user_name = k.find("a", {"class":"hnuser"})
+    try:
+      if user_name != None:
+        user_name = user_name.string
+        USER_NAME.append(user_name)
+      else:
+        user_name = None
+        USER_NAME.append(user_name)
+    except:
+      TypeError
+    finding_comments = k.find_all("a")
+    comments_num = finding_comments[-1]
+    try:
+      if comments_num != None:
+        short_link = comments_num.get("href")
+        SHORT_LINK.append(short_link)
+        comments_link = f"https://news.ycombinator.com/{short_link}"
+        COMMENTS_LINK.append(comments_link)
+        comments_num = comments_num.string
+        COMMENTS_NUM.append(comments_num)
+    except:
+      TypeError
+  
   all_info = []
-  number = len(HOUR)
-  x = range(number)
-  for i in x:
-    a = LOCATION[i]
-    b = COMPANY[i]
-    c = HOUR[i]
-    d = PAY[i]
-    e = UPLOADED[i]
-    info_dict = {"location":a, "company":b, "time":c, "pay":d, "date":e}
-    all_info.append(info_dict)
-  print(all_info)
+  for u in range(30):
+    add_link = links[u]
+    add_title = titles[u]
+    add_point = POINT[u]
+    add_user_name = USER_NAME[u]
+    add_comments_num = COMMENTS_NUM[u]
+    add_comments_link = COMMENTS_LINK[u]
+    add_short_link = SHORT_LINK[u]
+    add_dict = {"LINK":add_link, "TITLE":add_title, "POINT":add_point, "USER_NAME":add_user_name, "COMMENTS_NUM":add_comments_num, "COMMENTS_LINK":add_comments_link, "SHORT_LINK":add_short_link}
+    all_info.append(add_dict)
 
-
-  def save_to_file(w, all_info):
-    file = open(f"{w}.csv", encoding='utf-8-sig', mode="w")
-    writer = csv.writer(file)
-    writer.writerow(["title", "comapny", "location", "link"])
-    for i in all_info:
-      writer.writerow(list(i.values()))
     
-
-  save_to_file(w, all_info)
-
+  return all_info
 
 
+
+def extract_comments(COMMENTS_LINK):
+  USERS_COMMENTS =[]
+  for i in COMMENTS_LINK:
+    site_url = i.find_all("tr",{"class":"athing comtr "})
+    i = []
+    for z in site_url:
+      user = z.find("a", {"class":"hnuser"})
+      comment = z.find("span", {"class":"commtext c00"})
+      user_comment = {"user":user, "comment":comment}
+      i.append(user_comment)
+    USERS_COMMENTS.append(i)
+  print(USERS_COMMENTS)
+
+
+  
+  
+
+  
